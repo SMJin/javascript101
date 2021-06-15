b32a004d5464537dda10da78783f5c9aef754e85
SMJin <sstaw@naver.com>
2021-06-16 01:21:00 +0900

    event 위임 구현하기

--- 03.event/shopping-list_practice/main.js
+++ 03.event/shopping-list_practice/main.js
@@ -2,6 +2,8 @@ const addBtn = document.querySelector('.footer__button');
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 
+input.focus();
+
 function addList() {
     const text = input.value;
     if (text == '') {
@@ -12,28 +14,26 @@ function addList() {
     const item = createItem(text);
     items.appendChild(item);
     item.scrollIntoView({ block: 'center'});
-    
-    // button.addEventListener('click', () => {
-    //     items.removeChild(li);
-    //     items.removeChild(divisor);
-    // });
-
     input.value = '';
     input.focus();  
 };
 
+let id = 1;
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
+    itemRow.setAttribute('data-id', `${id}`);
     itemRow.innerHTML = `
         <div class="item">
             <span class="item__name">${text}</span>
             <button class="item__delete">
-                <i class="fas fa-trash-alt"></i>
+                <i class="fas fa-trash-alt" data-id=${id}></i>
             </button>
-            <div class="divisor"></div>
         </div>
+        <div class="divisor"></div>
     `;
+
+    id ++;
     return itemRow;
 }
 
@@ -44,5 +44,14 @@ addBtn.addEventListener('click', e => {
 input.addEventListener('keypress', e => {
     if (e.key == 'Enter') {
         addList();
+    }
+});
+
+items.addEventListener('click', e => {
+    const current__id = e.target.dataset.id;
+    if (current__id) {
+        console.log('delete 버튼이 눌렸습니다! id는 ' + current__id + '입니다.');
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${current__id}"]`);
+        items.removeChild(toBeDeleted);
     }
 });
