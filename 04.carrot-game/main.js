ceeb5d2f1f6accbcd597b2c982691c260be922e3
SMJin <sstaw@naver.com>
2021-06-19 23:23:02 +0900

    onload 로 getBoundingClientRect()가 height 값을 제대로 못받아오는 오류 수정

--- 04.carrot-game/main.js
+++ 04.carrot-game/main.js
@@ -2,29 +2,63 @@
 
 const IMG_SIZE = 730;
 const ITEM_COUNT = 5;
+
 const gameField = document.querySelector('.game__field');
 // getBoundingClientRect 를 사용하면 필드의 전체적인 사이즈와 포지션을 알 수 있다.
-const fieldRect = gameField.getBoundingClientRect();
+let fieldRect = undefined;
+window.onload = function(){
+    fieldRect = gameField.getBoundingClientRect();
+    console.log("fieldRect : width="+fieldRect.width+", height="+fieldRect.height);
+}
 const playBtn = document.querySelector('.play__btn');
 const stopBtn = document.querySelector('.stop__btn');
-const timer = document.querySelector('.timer');
-const score = document.querySelector('.game__score');
+const gameTimer = document.querySelector('.timer');
+const gameScore = document.querySelector('.game__score');
+
+
+let score = 0;
+let timer = undefined;
+
+playBtn.addEventListener('click', e => {
+    console.log('play button 이 눌렸습니다 !!');
+    console.log("fieldRect : width="+fieldRect.width+", height="+fieldRect.height);
+    
+    init();
+    gameTimer.classList.remove('timer--hide');
+    gameScore.classList.remove('game__score--hide');
+    playBtn.classList.add('play__btn--hide');
+    stopBtn.classList.remove('stop__btn--hide');
+});
+
+stopBtn.addEventListener('click', e => {
+    console.log('stop button 이 눌렸습니다 !!');
+    console.log("fieldRect : width="+fieldRect.width+", height="+fieldRect.height);
+    
+    gameTimer.classList.add('timer--hide');
+    gameScore.classList.add('game__score--hide');
+    playBtn.classList.remove('play__btn--hide');
+    stopBtn.classList.add('stop__btn--hide');
+    gameField.innerHTML = '';
+});
 
 function init() {
+    gameScore.innerHTML = ITEM_COUNT;
+    
     let repeat = 0;
     while (repeat < ITEM_COUNT) {
         createImg('./img/carrot.png', '50px', '50px', 'carrot');
         createImg('./img/bug.png', '50px', '50px', 'bug');
         repeat ++;
     }
+
+    startGameTimer();
 }
 
 function createImg(src, width, height, alt) {
     const x1 = 0;
     const y1 = 0;
-    const x2 = fieldRect.width - IMG_SIZE;
-    const y2 = fieldRect.right - 1321;
-    console.log("fieldRect : x="+x2+", y="+y2);
+    const x2 = fieldRect.width;
+    const y2 = fieldRect.height;
     
     const img = document.createElement('img');
     img.setAttribute('src', src);
@@ -35,7 +69,7 @@ function createImg(src, width, height, alt) {
     img.style.position = 'absolute';
     const x = randomNumber(x1, x2);
     const y = randomNumber(y1, y2);
-    console.log("left : " + x +", top : " + y);
+//    console.log("left : " + x +", top : " + y);
     img.style.left = `${x}px`;
     img.style.top = `${y}px`;
 
@@ -46,11 +80,6 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-playBtn.addEventListener('click', e => {
-    console.log('play button 이 눌렸습니다 !!');
-    init();
-    timer.classList.remove('timer--hide');
-    score.classList.remove('game__score--hide');
-    playBtn.classList.add('play__btn--hide');
-    stopBtn.classList.remove('stop__btn--hide');
-});
+function startGameTimer() {
+
+}
