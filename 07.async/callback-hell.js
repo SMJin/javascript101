77931bda55056f4b266295ac9db3d3c3b038ce6f
SMJin <sstaw@naver.com>
2021-07-18 19:00:23 +0900

    edit callback-hell.js

--- 07.async/callback-hell.js
+++ 07.async/callback-hell.js
@@ -8,19 +8,42 @@ class UserStorage {
                 (id === 'you' && password === 'right')
             ) {
                 onSuccess(id);
+                console.log(id.name);
             } else {
                 onError(new Error('not found'));
             }
         }, 2000);
     }
+
+    getRoles(user, onSuccess, onError) {
+        setTimeout(() => {
+            if (user === 'mj') {
+                onSuccess({ name :'mj', role: 'admin'});
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
 }
 
-getRoles(user, onSuccess, onError) {
-    setTimeout(() => {
-        if (user === 'mj') {
-            onSuccess({ name :'mj', role: 'admin'});
-        } else {
-            onError(new Error('no access'));
-        }
-    }, 1000);
-}
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+    id, 
+    password, 
+    user => {
+        userStorage.getRoles(
+            user, 
+            userWithRole => {
+                alert(`Hello, ${userWithRole.name}, you have a ${userWithRole.role} role.`);
+            },
+            error => { 
+                console.log(error); 
+            }
+        );
+    },
+    error => { 
+        console.log(error); 
+    }
+);
